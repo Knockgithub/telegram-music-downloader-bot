ef505023fc3963879938d5097e7fb650ce7f827c
Knockgithub <131382142+Knockgithub@users.noreply.github.com>
2023-04-25 20:10:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from os.path import join, dirname
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-TOKEN = os.environ.get("TOKEN")
+TOKEN = os.environ.get("6261503028:AAHolzo2ScPLpznYyxHkPN3rFLP3snqEDjw")
 bot = telepotpro.Bot(TOKEN)
 
 class Music:
